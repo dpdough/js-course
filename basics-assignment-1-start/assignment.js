a9de6a20ee6ade89a0247f9042cd6bd2be2170f2
damianpawlowski <pawlowski.coinbase@gmail.com>
2022-09-26 19:55:33 +0200

    Added new folders to repo

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,9 @@
+const fictionalUserInput = 11;
+let resultVeriable;
+resultVeriable = fictionalUserInput + 18;
+resultVeriable = resultVeriable - 3;
+resultVeriable = resultVeriable * 2;
+resultVeriable = resultVeriable / 3;
+alert(resultVeriable);
+alert(fictionalUserInput);
+
